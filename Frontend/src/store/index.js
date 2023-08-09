a8840d32e24ad282114d9f6f6e879fcf572d86e8
Hyungchul You <teoyou881@gmail.com>
2023-08-09 07:29:22 -0400

    set redux-persist

--- Frontend/src/store/index.js
+++ Frontend/src/store/index.js
@@ -1,8 +1,52 @@
-import { configureStore } from "@reduxjs/toolkit";
+import { combineReducers, configureStore } from "@reduxjs/toolkit";
 import userReducer from "./userSlice";
+import storage from "redux-persist/lib/storage";
+import {
+    DEFAULT_VERSION,
+    FLUSH,
+    KEY_PREFIX,
+    PAUSE,
+    PERSIST,
+    PURGE,
+    REGISTER,
+    REHYDRATE,
+    persistReducer,
+    persistStore,
+} from "redux-persist";
+
+// Takes an object with values from different reducing functions and
+// converts them into a single reducing function that can be passed to the createstore.
+export const rootReducer = combineReducers({
+    user: userReducer,
+});
+
+// Decide where to store the state in your redux store.
+const persistConfig = {
+    key: "root", // key 이름
+    storage, // save to localStorage
+    //whiteList: [userReducer], // Among multiple reducers, only the corresponding reducer is stored in localstorage.
+    // blackList: [] // blacklist -> Except for this.
+};
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
 
 export const store = configureStore({
-    reducer: {
-        user: userReducer,
-    },
+    reducer: persistedReducer,
+    middleware: (getDefaultMiddleware) =>
+        getDefaultMiddleware({
+            serializableCheck: {
+                ignoredActions: [
+                    KEY_PREFIX,
+                    FLUSH,
+                    REHYDRATE,
+                    PAUSE,
+                    PERSIST,
+                    PURGE,
+                    REGISTER,
+                    DEFAULT_VERSION,
+                ],
+            },
+        }),
 });
+
+export const persistor = persistStore(store);
