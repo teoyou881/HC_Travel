c3baa24eea179eb3770197a1de85cf500dc95277
Hyungchul You <teoyou881@gmail.com>
2023-08-09 03:31:23 -0400

    build a CSS overall structure. use react-icons

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,15 +1,48 @@
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
+import { Outlet, Route, Routes } from "react-router-dom";
 import "./App.css";
-import { useState } from "react";
+import LoginPage from "./pages/LoginPage/index";
+import RegisterPage from "./pages/RegisterPage/index";
+import LandingPage from "./pages/LandingPage/index";
+import NotAuthRoutes from "./components/NotAuthRoutes";
+import ProtectedRoutes from "./components/ProtectedRoutes";
+import Navbar from "./layout/Navbar";
+import Footer from "./layout/Footer";
+
+function Layout() {
+    return (
+        <div className="flex flex-col justify-between h-screen">
+            <Navbar />
+            <main className="mb-auto w-10/12 max-w-4xl mx-auto">
+                <Outlet />
+            </main>
+            <Footer />
+        </div>
+    );
+}
 
 function App() {
-    const [count, setCount] = useState(0);
-
     return (
-        <>
-            <h1 className="underline text-3xl bg-blue-200">Hello World</h1>
-        </>
+        <Routes>
+            <Route path="/" element={<Layout />}>
+                {/* Paths you can take regardless of login  */}
+                <Route index element={<LandingPage />}></Route>
+
+                {/* Paths that can't be taken by logged in users */}
+                {/* <Route element={<NotAuthRoutes />}> */}
+                <Route path="/login" element={<LoginPage />}></Route>
+                <Route path="/register" element={<RegisterPage />}></Route>
+                {/* </Route> */}
+
+                {/* Paths that only signed-in people can take */}
+                {/* todo */}
+                {/* <Route element={<ProtectedRoutes/>}>
+                    <Route path='/product/upload' element={<UploadProductPage/>}/>
+                    <Route path='/product/:productId' element={<DetailProductPage/>}/>
+                    <Route path='/history' element={<HistoryPage/>}/>
+                    <Route path='/user/cart' element={<CartPage/>}/>
+                </Route> */}
+            </Route>
+        </Routes>
     );
 }
 
---
+++ Frontend/src/layout/Footer/index.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { FaRegSmileBeam } from "react-icons/fa";
+
+function Footer() {
+    return (
+        <div className="flex h-20 text-lg justify-center items-center">
+            All rights reserved. <FaRegSmileBeam />
+        </div>
+    );
+}
+
+export default Footer;
