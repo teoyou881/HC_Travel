f465c47043add085047833878f32e72a0c75ba68
Hyungchul You <teoyou881@gmail.com>
2023-08-29 19:31:13 -0400

    At front, detail product send data to back

--- Frontend/src/pages/DetailProductPage/index.jsx
+++ Frontend/src/pages/DetailProductPage/index.jsx
@@ -1,6 +1,23 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import axiosInstance from "../../utills/axios";
 
 function DetailProductPage() {
+    const { productId } = useParams();
+    const [product, setProduct] = useState(null);
+
+    useEffect(() => {
+        async function fetchProduct() {
+            try {
+                const response = await axiosInstance.get(`/products/${productId}?type=single`);
+                console.log(response);
+                setProduct(response.data[0]);
+            } catch (e) {
+                console.log(e);
+            }
+        }
+    }, [productId]);
+
     return <div>DetailProductPage</div>;
 }
 
