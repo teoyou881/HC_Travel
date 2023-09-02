766374b08272a54d877b10070c5700f30b9de862
Hyungchul You <teoyou881@gmail.com>
2023-09-02 17:01:35 -0400

    At front, build userCart page

---
+++ Frontend/src/pages/CartPage/Sections/CartTable.jsx
@@ -0,0 +1,48 @@
+import React from "react";
+
+const CartTable = ({ products, onRemoveItem }) => {
+    const renderCartImage = (images) => {
+        if (images.length > 0) {
+            let image = images[0];
+            return image;
+        }
+    };
+
+    const renderItems =
+        products.length > 0 &&
+        products.map((product) => (
+            <tr key={product._id}>
+                <td>
+                    {/* products.images=[12312,412314,12314] 
+                    Among them, I will use first iamge
+                    Check if there is no images in product*/}
+                    <img
+                        className="w-[160px]"
+                        src={renderCartImage(product.images)}
+                        alt="product"
+                    />
+                </td>
+                <td>{product.quantity}</td>
+                <td>{product.price} $</td>
+                <td>
+                    <button onClick={() => onRemoveItem(product._id)}> Delete</button>
+                </td>
+            </tr>
+        ));
+
+    return (
+        <table className="w-full text-sm text-left">
+            <thead className=" border-[1px]">
+                <tr>
+                    <th>Imgae</th>
+                    <th>Quantity</th>
+                    <th>Price</th>
+                    <th>Delete</th>
+                </tr>
+            </thead>
+            <tbody>{renderItems}</tbody>
+        </table>
+    );
+};
+
+export default CartTable;
--- Frontend/src/pages/CartPage/index.jsx
+++ Frontend/src/pages/CartPage/index.jsx
@@ -1,10 +1,13 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { getCartItems } from "../../store/thunkFunctions";
+import CartTable from "./Sections/CartTable";
 
 function CartPage() {
     const dispatch = useDispatch();
     const userData = useSelector((state) => state.user?.userData);
+    const cartDetail = useSelector((state) => state.user?.cartDetail);
+    const [total, setTotal] = useState(0);
 
     useEffect(() => {
         let cartItemIds = [];
@@ -24,7 +27,43 @@ function CartPage() {
         }
     }, [dispatch, userData]);
 
-    return <div>CartPage</div>;
+    useEffect(() => {}, [cartDetail]);
+
+    const calculateTotal = (cartProducts) => {
+        let totalPrice = 0;
+        cartProducts.map((product) => (totalPrice += product.price * product));
+        setTotal(total);
+    };
+
+    const handlePaymentClick = () => {};
+
+    const handleRemoveCartItem = () => {};
+
+    return (
+        <section>
+            <div className="text-center m-7">
+                <h2 className="text-2xl">Shopping Cart</h2>
+            </div>
+
+            {cartDetail?.length > 0 ? (
+                <>
+                    <CartTable products={cartDetail} onRemoveItem={handleRemoveCartItem} />
+                    <div className="mt-10">
+                        <p>
+                            <span className="font-bold">Total:</span> {total} $
+                        </p>
+                        <button
+                            className="text-white bg-black hover:bg-gray-500 rounded-lg px-4 py-2 mt-5"
+                            onClick={handlePaymentClick}>
+                            Proceed to Checkout
+                        </button>
+                    </div>
+                </>
+            ) : (
+                <p> Your HC Cart is empty.</p>
+            )}
+        </section>
+    );
 }
 
 export default CartPage;
