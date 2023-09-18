b6f59725211c67e17c04be8a502af1db28351c45
Hyungchul You <teoyou881@gmail.com>
2023-09-18 01:20:47 -0400

    build span tag showing the quantity in history

--- Frontend/src/pages/HistoryPage/index.jsx
+++ Frontend/src/pages/HistoryPage/index.jsx
@@ -1,6 +1,7 @@
 import React, { useEffect, useLayoutEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { Types } from "../../store/user";
+import { AiOutlineShoppingCart } from "react-icons/ai";
 
 function HistoryPage() {
     const userData = useSelector((state) => state.user?.userData);
@@ -37,14 +38,21 @@ function HistoryPage() {
                                         // const orderData = new Date(orderHistory.).toISOString().slice(0, 10);
                                         <div key={item.id} className="mb-2 last:mb-0">
                                             <div className="flex justify-start">
-                                                <div className="w-[100px] h-[90px]">
+                                                <div className="w-[100px] h-[90px] relative">
                                                     {/* TODO
                                                     when clicking image, go to product detail */}
-                                                    <img
-                                                        src={item.image}
-                                                        className="w-[90px] h-[90px]"></img>
+                                                    <a>
+                                                        <img
+                                                            src={item.image}
+                                                            className="w-[90px] h-[90px]"></img>
+                                                    </a>
+                                                    {item.quantity > 1 && (
+                                                        <span className="absolute top-[71px] left-[93px] inline-flex items-center justify-center w-5 h-5 text-xs font-thin text-black bg-white border-[1px] border-gray-400 rounded-full">
+                                                            {item.quantity}
+                                                        </span>
+                                                    )}
                                                 </div>
-                                                <div>description</div>
+                                                <div className="pl-4">description</div>
                                             </div>
                                         </div>
                                     ))}
