ed484b04b24f2b63a301c70b198ae9ff0553d204
Hyungchul You <teoyou881@gmail.com>
2023-12-05 18:23:05 -0500

    set bacurl

--- Frontend/config/config.js
+++ Frontend/config/config.js
@@ -1,1 +1,2 @@
-export const backUrl = process.env.NODE_ENV === 'production' ? 'https://port-0-hc-travel-32updzt2alpstkco5.sel4.cloudtype.app/' : 'http://localhost:4000';
+// export const backUrl = process.env.NODE_ENV === 'production' ? 'https://port-0-hc-travel-32updzt2alpstkco5.sel4.cloudtype.app/' : 'http://localhost:4000';
+export const backUrl ='https://port-0-hc-travel-32updzt2alpstkco5.sel4.cloudtype.app/';
